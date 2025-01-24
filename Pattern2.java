affdaf0a9b4f38e974baf697a72e03043ff7de22
laxman <laxmanreddy2820@gmail.com>
2025-01-24 14:55:50 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,19 @@
+package MyProject;
+
+public class Pattern2 {
+    public static void main(String[] args) {
+        int sp = 10;
+        
+        for (int r = 1; r <=13; r++) {
+          
+            for (int k = 1; k <= sp; k++) {
+                System.out.print(" ");             }
+            sp = sp - 1;
+            for (int c = 1; c <= r; c++) {
+                System.out.print(r+" ");
+            }
+           
+            System.out.println();
+        }
+    }
+}
